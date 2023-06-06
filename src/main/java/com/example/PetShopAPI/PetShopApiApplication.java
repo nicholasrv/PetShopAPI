7ee4194f635b8d274f3586a5181386aa74256d53
Nicholas Ruas Viegas <nicholasrv@live.com>
2023-06-05 22:18:41 -0300

    Setting up repository and path

---
+++ src/main/java/com/example/PetShopAPI/PetShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetShopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetShopAPI/PetShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetShopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
